722586357796ffa9ad33f53d66af8ddb35e9cf4e
Filipe Guimarães <fmtfg99@gmail.com>
2020-03-25 14:48:18 +0000

    14 48

--- src/AnonGw.java
+++ src/AnonGw.java
@@ -1,20 +1,40 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AnonGw {
 
+    private static int local_TCP_port = 80;
+    private static int UDP_port = 6666;
+    
     public static void main(String[] args){
-
         int argc = args.length;
-        String target_server = args[1];
-        int port = Integer.parseInt(args[3]);
-        List<String> overlay_peers = new ArrayList<>(Arrays.asList(args).subList(5, argc));
+        if(argc>=4) {
+            String target_server = args[1];
+            int port = Integer.parseInt(args[3]);
+            //List<String> overlay_peers = new ArrayList<>(Arrays.asList(args).subList(5, argc));
+            //System.out.println(overlay_peers.toString());
 
-        for(String s : overlay_peers){
-            System.out.println(s);
-        }
+            try {
+                InetAddress this_address = InetAddress.getByName("127.0.0.1");
+                System.out.println(this_address.toString());
+            } catch (UnknownHostException e) {
+                System.out.println("Não conseguiu obter o endereço. Error: "+e.getMessage());
+            }
+/*
+            try {
+                ServerSocket welcomeSocket = new ServerSocket(local_TCP_port);
+                welcomeSocket.accept();
 
+                Socket socketSaida= new Socket(target_server, 80);
+            } catch (IOException e) {
+                System.out.println("Connection TCP error: " + e.getMessage());
+            }
+
+*/
+        } else System.out.println("Not enough arguments.");
 
 
     }
