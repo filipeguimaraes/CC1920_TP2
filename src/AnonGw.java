5b42ac50393cfd99ce392b34532292ce0d3a80a5
Filipe Guimarães <fmtfg99@gmail.com>
2020-04-07 11:52:34 +0100

    duvido

--- src/AnonGw.java
+++ src/AnonGw.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -21,6 +22,8 @@ public class AnonGw {
             overlay_peers = new ArrayList<>(Arrays.asList(args).subList(5, argc));
 
             try {
+
+                //Pedido
                 ServerSocket anon = new ServerSocket(local_TCP_port);
                 System.out.println("Anon iniciado na porta " + local_TCP_port);
 
@@ -29,8 +32,11 @@ public class AnonGw {
                 System.out.println("Cliente conectado do IP " +
                         client_socket.getInetAddress().getHostAddress());
 
+                String clienteIP = client_socket.getInetAddress().getHostAddress();
+
                 Socket server_socket = new Socket(target_server, local_TCP_port);
-                System.out.println("Servidor" + target_server + " conectado na porta " + local_TCP_port);
+                System.out.println("Servidor" + target_server +
+                            " conectado na porta " + local_TCP_port);
 
                 OutputStream output = server_socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
@@ -51,6 +57,38 @@ public class AnonGw {
                 }
 
 
+
+
+                //Resposta
+
+
+                Socket server_socket_response = anon.accept();
+                System.out.println("Servidor conectado"+
+                        client_socket.getInetAddress().getHostAddress());
+
+                Socket client_socket_response = new Socket(clienteIP, local_TCP_port);
+                System.out.println("cliente a escutar");
+
+                OutputStream output_resp = client_socket_response.getOutputStream();
+                PrintWriter writer_resp = new PrintWriter(output_resp, true);
+
+                Scanner entrada_resp = new Scanner(server_socket_response.getInputStream());
+                while (entrada_resp.hasNextLine()) {
+                    writer_resp.println(entrada_resp.nextLine());
+                }
+
+                InputStream input_resp = client_socket_response.getInputStream();
+
+                BufferedReader reader_resp = new BufferedReader(new InputStreamReader(input_resp));
+
+                String line_resp;
+
+                while ((line_resp = reader_resp.readLine()) != null) {
+                    System.out.println(line_resp);
+                }
+
+
+                //Fechar conexoes
                 client_socket.close();
                 entrada.close();
                 server_socket.close();
